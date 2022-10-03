23b10d6734e868afc81ac82609d0d9933bf76a6f
Jonathan Walter <jonathan.walter@eyevinn.se>
2022-10-03 15:49:45 +0200

    feat: add support for dotenv, add nodemon for dev (#25)

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import { AutoabrService } from './src/api/AutoabrService';
 
+require('dotenv').config()
+
 // initialize a new instance of AutoabrService
 const autoabrService = new AutoabrService();
 // register the routes
