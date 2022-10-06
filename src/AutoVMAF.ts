80b653cc45373d4e04733d40e45eaf31c6c919d6
Oscar Nord <oscar.nord@gmail.com>
2022-10-06 22:31:10 +0200

    Initial support for Redis

--- src/AutoVMAF.ts
+++ src/AutoVMAF.ts
@@ -1,6 +1,7 @@
 import { createJob as createAutoVMAFJob, getVmaf, logger, QualityAnalysisModel } from '@eyevinn/autovmaf';
 import { S3, GetObjectCommand } from '@aws-sdk/client-s3';
 import { nanoid } from 'nanoid';
+import { RedisConnector } from './RedisConnector';
 
 export enum State {
   IDLE = 'IDLE',
@@ -13,12 +14,32 @@ export class AutoABR {
   private jobStatus: State;
   private startTime = new Date();
   private endTime = new Date();
-  private latestJobOutput: string;
+  private jobData: any;
+  private encodingProfileData: any;
+  private pipelineData: any;
+  private redis: RedisConnector;
 
   constructor() {
+    this.redis = new RedisConnector();
     this.instanceId = nanoid();
     this.jobStatus = State.INACTIVE;
-    this.latestJobOutput = '';
+    this.jobData = {};
+    this.encodingProfileData = {};
+    this.pipelineData = {};
+  }
+
+  public async updateRedis() {
+    await this.redis.set(
+      this.instanceId,
+      JSON.stringify({
+        id: this.instanceId,
+        status: this.jobStatus,
+        jobData: this.jobData,
+        encodingProfileData: this.encodingProfileData,
+        pipelineData: this.pipelineData,
+        jobTimer: this.getJobTimer(),
+      })
+    );
   }
 
   get id(): string {
@@ -30,18 +51,16 @@ export class AutoABR {
   }
 
   get jobOutput(): string {
-    return this.latestJobOutput;
+    return this.jobData['output'];
   }
 
   set status(status: State) {
-    if (Object.values(State).includes(status)) {
-      this.jobStatus = status;
-    }
+    this.jobStatus = status;
   }
 
   getJobTimer(): number {
     if (this.jobStatus === State.ACTIVE) {
-      return (new Date()).getTime() - this.startTime.getTime();
+      return new Date().getTime() - this.startTime.getTime();
     }
     return this.endTime.getTime() - this.startTime.getTime();
   }
@@ -51,27 +70,41 @@ export class AutoABR {
     const s3bucket = S3url.split('/')[2];
     const s3key = S3url.split('/').slice(3).join('/');
     console.log(`Bucket: ${s3bucket}, Key: ${s3key}`);
-    const getCommand = new GetObjectCommand({ Bucket: `${s3bucket}`, Key: `${s3key}` });
+    const getCommand = new GetObjectCommand({
+      Bucket: `${s3bucket}`,
+      Key: `${s3key}`,
+    });
     const response = await s3.send(getCommand);
     return await this.streamToString(response);
   }
 
-  public async createJob(jobData: any, pipelineData: any, encodingProfileData: any) {
-    this.latestJobOutput = jobData['output'];
+  public async createJob(
+    jobData: any,
+    pipelineData: any,
+    encodingProfileData: any
+  ) {
+    this.jobData = jobData;
+    this.encodingProfileData = encodingProfileData;
+    this.pipelineData = pipelineData;
     this.start(jobData, pipelineData, encodingProfileData);
   }
 
-  public async getJobOutput(outputFolder?: string, model?: string): Promise<{}> {
+  public async getJobOutput(
+    outputFolder?: string,
+    model?: string
+  ): Promise<{}> {
     let output = {};
-    let folder = outputFolder ? outputFolder : this.latestJobOutput;
-    if(!folder || this.status === State.ACTIVE) return output;
+    let folder = outputFolder ? outputFolder : this.jobData['output'];
+    if (!folder || this.status === State.ACTIVE) return output;
     this.jobStatus = State.ACTIVE;
     output[folder] = {};
     if (model) {
       try {
-        const vmafFiles = await getVmaf(`s3://vmaf-files/results/encoded-files/${folder}/${model}/`);
-        output[folder][model] = {}
-        vmafFiles.map(file => {
+        const vmafFiles = await getVmaf(
+          `s3://vmaf-files/results/encoded-files/${folder}/${model}/`
+        );
+        output[folder][model] = {};
+        vmafFiles.map((file) => {
           output[folder][model][file['filename']] = file['vmaf'];
         });
         this.jobStatus = State.INACTIVE;
@@ -82,12 +115,16 @@ export class AutoABR {
         throw error;
       }
     } else {
-      const models = Object.keys(QualityAnalysisModel).filter((v) => isNaN(Number(v)));
+      const models = Object.keys(QualityAnalysisModel).filter((v) =>
+        isNaN(Number(v))
+      );
 
       for (const model in models) {
-        output[folder][models[model]] = {}
-        const vmafFiles = await getVmaf(`s3://vmaf-files/results/encoded-files/${folder}/${models[model]}/`);
-        vmafFiles.map(file => {
+        output[folder][models[model]] = {};
+        const vmafFiles = await getVmaf(
+          `s3://vmaf-files/results/encoded-files/${folder}/${models[model]}/`
+        );
+        vmafFiles.map((file) => {
           output[folder][models[model]][file['filename']] = file['vmaf'];
         });
       }
@@ -96,9 +133,14 @@ export class AutoABR {
     }
   }
 
-  private async start(jobData: any, pipelineData: any, encodingProfileData: any) {
+  private async start(
+    jobData: any,
+    pipelineData: any,
+    encodingProfileData: any
+  ) {
     this.jobStatus = State.ACTIVE;
     this.startTime = new Date();
+    await this.updateRedis();
     await createAutoVMAFJob(jobData, pipelineData, encodingProfileData);
     this.endTime = new Date();
     this.jobStatus = State.INACTIVE;
---
+++ src/RedisConnector.ts
@@ -0,0 +1,33 @@
+import { createClient } from 'redis';
+
+export class RedisConnector {
+  private redisClient: any;
+
+  constructor() {
+    this.redisClient = createClient();
+    this.redisClient.on('error', (err: any) => {
+      console.log('Error ' + err);
+    });
+    this.redisClient.connect();
+  }
+
+  public async get(key: string): Promise<string> {
+    return await this.redisClient.get(key);
+  }
+
+  public async set(key: string, value: string): Promise<void> {
+    await this.redisClient.set(key, value);
+  }
+
+  public async delete(key: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.redisClient.del(key, (err: any, reply: any) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+}
